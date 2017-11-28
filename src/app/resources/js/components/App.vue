c6a6dba318a3fc7ac11721c493d862d21c62f615
Anthony Porthouse <anthony.porthouse@opia-sp.com>
2017-11-28 13:20:34 +0000

    Extract Sidebar into its own component for initialization

--- src/app/resources/js/components/App.vue
+++ src/app/resources/js/components/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     <h1>{{ message }}</h1>
-    <sidebar :list="getSidebar"></sidebar>
+    <sidebar></sidebar>
     <feeds></feeds>
   </div>
 
 </template>
 
 <script>
-  import { mapGetters, mapActions } from 'vuex';
-
   import Feeds from './Feeds.vue';
-  import Sidebar from './Sidebar/List.vue';
+  import Sidebar from './Sidebar/Sidebar.vue';
   import api from "../api/api";
 
   export default {
@@ -28,17 +26,7 @@
       'sidebar': Sidebar,
     },
 
-    computed: {
-      ...mapGetters([
-        'getSidebar',
-      ]),
-    },
-
     methods: {
-      ...mapActions([
-        'getSidebarItems',
-      ]),
-
       getStatus() {
         api.getStatus((status) => {
           this.status = status;
@@ -48,7 +36,6 @@
 
     beforeMount() {
       this.getStatus();
-      this.getSidebarItems();
     },
   }
 </script>
---
+++ src/app/resources/js/components/Sidebar/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <sidebar-list :list="getSidebar"></sidebar-list>
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex';
+  import api from "../../api/api";
+  import SidebarList from './List.vue';
+
+  export default {
+    components: {
+      'sidebar-list': SidebarList,
+    },
+
+    computed: {
+      ...mapGetters([
+        'getSidebar',
+      ]),
+    },
+
+    methods: {
+      ...mapActions([
+        'getSidebarItems',
+      ]),
+    },
+
+    beforeMount() {
+      this.getSidebarItems();
+    }
+  }
+</script>
